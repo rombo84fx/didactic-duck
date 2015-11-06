e96ce4ea7df4ed5bd13464f3bd10d3beef3f43e8
rombo84fx <gmart_nez@yahoo.com.mx>
2015-11-05 22:00:24 -0600

    Version copied from edX

--- ModuleTwo/ModuleTwo/Program.cs
+++ ModuleTwo/ModuleTwo/Program.cs
@@ -8,37 +8,18 @@ namespace ModuleTwo
         {
             for (int i = 0; i < 8; i++)
             {
-                if (i % 2 == 0)
+                for (int j = 0; j < 8; j++)
                 {
-                    for (int j = 0; j < 8; j++)
+                    if ((i + j) % 2 == 0)
                     {
-                        if (j % 2 == 0)
-                        {
-                            Console.Write("X");
-                        }
-                        else
-                        {
-                            Console.Write("O");
-                        }
+                        Console.Write("X");
                     }
-                    Console.Write("\n");
-                }
-                else
-                {
-                    for (int j = 0; j < 8; j++)
+                    else
                     {
-                        if (j % 2 == 0)
-                        {
-                            Console.Write("O");
-                        }
-                        else
-                        {
-                            Console.Write("X");
-                        }
+                        Console.Write("O");
                     }
-                    Console.Write("\n");
                 }
-
+                Console.Write("\n");
             }
             Console.ReadKey();
         }
